237f4d7669d01ff90e6b20a3fc38266116145802
Daniel Chavez <dsnp010923@ugb.edu.sv>
2023-09-22 22:58:34 -0600

    Agregar archivos de proyecto.

---
+++ Module1.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main(Args As String())
+
+
+        Dim aux1, suma, n, i As Integer
+        Dim bandera As Boolean
+        bandera = False
+
+        While (bandera = False)
+
+
+            Console.Clear()
+            suma = 0
+            Console.WriteLine("Ingrese el valor de la tabla a calcular")
+            n = Console.ReadLine()
+
+            If (n <= 0) Then
+                Console.WriteLine("Por favor ingrese un numero mayor que 0")
+                bandera = False
+            Else
+
+
+                For i = 1 To 10
+                    Console.WriteLine(n & "X" & i & "=" & n * i)
+                    suma = suma + i
+                Next
+
+                Console.WriteLine("Si desea ingresar otro numero presione 1 sino cualquier tecla")
+                aux1 = Console.ReadLine()
+                If (aux1 = 1) Then
+                    bandera = False
+                Else
+                    bandera = True
+                End If
+            End If
+        End While
+    End Sub
+
+End Module
